ed8df847d5dc8fbe12d115c4027f55d16994359f
Logan Alvarez <logan.alvarez616@gmail.com>
2021-11-19 17:04:48 -0500

    Changed JS file name

--- myfile.js
+++
@@ -1,6 +0,0 @@
-let person = "Mitch Cuckovich"
-let age = 25
-let birthday = ("January 24")
-let pineapplePizza = 0
-lifeEvents = ["I was born in Troy, Michigan.","I went to Hope College.", "I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.", "I'm a graduate of a Grand Circus bootcamp."]
-
